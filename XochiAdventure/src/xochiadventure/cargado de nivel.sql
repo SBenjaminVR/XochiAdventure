249bdfebf782fb390dac488539d73cf2711aee1d
albgv1402 <betinbanana@hotmail.com>
2019-05-07 00:27:59 -0500

    Hice los querries dentro de java para poder usar los datos dentro del juego. No lo he implementado, solo están hechos

---
+++ XochiAdventure/src/xochiadventure/cargado de nivel.sql
@@ -0,0 +1,19 @@
+-- Nivel 1 ----------------------------------------------------------------
+
+SELECT *
+FROM LevelG
+
+SELECT *
+FROM Fountain
+
+SELECT *
+FROM Player
+
+SELECT *
+FROM Platform
+
+SELECT *
+FROM Comida
+
+SELECT *
+FROM Chile
