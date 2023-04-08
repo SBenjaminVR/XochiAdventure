7920a238fb4f55ef1b9cb7e96a06c52752f26f91
alegayndra <alegayndra@gmail.com>
2023-04-08 10:37:33 -0600

    Movi todo te folder

--- xochiadventure/xochiadventure/XochiAdventure.java
+++ xochiadventure/xochiadventure/XochiAdventure.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xochiadventure;
 
 /**
@@ -14,6 +9,7 @@ package xochiadventure;
  *      - Benjamín Váldez Rodríguez             A00822027
  */
 public class XochiAdventure {
+
   /**
    * @param args the command line arguments
    */
